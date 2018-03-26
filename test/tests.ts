23d992ae623efe09e195ae31f2bf8ba7e62b50a0
Jeremy Foster <jeremy.foster@live.com>
2018-03-26 18:38:49 -0400

    add basic test framework

---
+++ test/tests.ts
@@ -0,0 +1,16 @@
+import 'mocha';
+import * as assert from 'assert';
+import 'chai';
+
+import { HubClient } from "..";
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+describe('Connect to IoT Hub', async function () {
+  // const iothub = new HubClient(process.env.DEVICE_CONN_STRING);
+  // let ready = await iothub.ready;
+  throw "Not yet implemented";
+  
+});
+
