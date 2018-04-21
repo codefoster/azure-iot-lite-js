f711465a4887e32532504881b2215b958a51e7de
Jeremy Foster <jeremy.foster@live.com>
2018-04-21 19:36:37 +1000

    add some test stubs

--- src/tests.ts
+++ src/tests.ts
@@ -18,13 +18,22 @@ describe('Connect to IoT Hub', function() {
 
 describe('Send messages', function() {
   it('Should send a D2C message', async function() {
+    this.timeout(5000);
     const iothub = new HubClient(process.env.DEVICE_CONN_STRING);
     await iothub.ready;
     iothub.sendMessage('test');
     return true;
   });
-
+  
   it('Should receive a C2D message');
-
+  
 });
 
+describe('Device twin properties', function() {
+  it('Should receive desired properties');
+  it('Should send reported properties');
+})
+
+describe('Direct methods', function() {
+  it('Should invoke direct methods');
+})
